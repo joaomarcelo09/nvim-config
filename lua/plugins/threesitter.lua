f861ec57d3ae4acd90f2f4968175165f6dc155ef
Joao Gomes <jg0112360@gmail.com>
2025-05-09 02:40:42 -0300

    update nvim

--- lua/plugins/threesitter.lua
+++ lua/plugins/threesitter.lua
@@ -12,24 +12,33 @@ require'nvim-treesitter.configs'.setup {
       enable = true,
       lookahead = true, -- Automatically jump forward to matching textobj
       keymaps = {
+        -- Around and Inside functions
         ["af"] = "@function.outer", -- around function
         ["if"] = "@function.inner", -- inside function
+
+        -- Around and Inside classes
         ["ac"] = "@class.outer",    -- around class
         ["ic"] = "@class.inner",    -- inside class
+
+        -- Around and Inside HTML/TSX tags
+        ["at"] = "@tag.outer",      -- around tag (e.g., <div>...</div>)
+        ["it"] = "@tag.inner",      -- inside tag (e.g., content inside <div>)
       },
     },
+
     move = {
       enable = true,
       set_jumps = true, -- keep jump history
       goto_next_start = {
-        ["]f"] = "@function.outer",
-        ["]c"] = "@class.outer",
+        ["]f"] = "@function.outer", -- move to next function
+        ["]c"] = "@class.outer",    -- move to next class
+        ["]t"] = "@tag.outer",      -- move to next tag (e.g., <div>)
       },
       goto_previous_start = {
-        ["[f"] = "@function.outer",
-        ["[c"] = "@class.outer",
+        ["[f"] = "@function.outer", -- move to previous function
+        ["[c"] = "@class.outer",    -- move to previous class
+        ["[t"] = "@tag.outer",      -- move to previous tag (e.g., <div>)
       },
     },
   },
 }
-
