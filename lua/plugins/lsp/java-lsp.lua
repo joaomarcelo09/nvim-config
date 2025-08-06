40d2342fc90541ceb444c312f8ccd56fe008b40a
Joao Gomes <joao.gomes@isdbflowtech.com>
2025-08-06 11:09:40 -0300

    feat: lsp java with lombok

--- lua/plugins/lsp/java-lsp.lua
+++ lua/plugins/lsp/java-lsp.lua
@@ -2,6 +2,10 @@ local jdtls = require("jdtls")
 
 -- Detect project name & folder
 local home = os.getenv("HOME")
+
+-- Set up environment variable for JDTLS with lombok
+vim.env.JDTLS_JVM_ARGS = "-javaagent:" .. home .. "/.local/share/java/lombok.jar"
+
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = home .. "/.local/share/eclipse/" .. project_name
 
@@ -11,13 +15,28 @@ local on_attach = function(client, bufnr)
   vim.highlight.priorities.semantic_tokens = 95
 end
 
+cmd_config = {
+  "java",
+  "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+  "-Dosgi.bundles.defaultStartLevel=4",
+  "-Declipse.product=org.eclipse.jdt.ls.core.product",
+  "-Dlog.protocol=true",
+  "-Dlog.level=ALL",
+  "-javaagent:" .. home .. "/.local/share/java/lombok.jar",
+  "-Xms1g",
+  "--add-modules=ALL-SYSTEM",
+  "--add-opens", "java.base/java.util=ALL-UNNAMED",
+  "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+  "-jar", vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"):gsub("\n", ""),
+  "-configuration", home .. "/.local/share/nvim/mason/packages/jdtls/config_linux",
+  "-data", workspace_dir
+}
+
 -- Build config
 local config = {
-  cmd = { "jdtls" },
+  cmd = cmd_config,
   root_dir = require("lspconfig").util.root_pattern(".git", "mvnw", "gradlew", "pom.xml", "build.gradle"),
-
   on_attach = on_attach, -- Attach our magic when LSP starts
-
   settings = {
     java = {
       format = { enabled = true },
